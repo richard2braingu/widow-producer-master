87f9c84ad443c4be73ed6aea358051c49b833f22
Richard Pajerski <richard.pajerski@braingu.com>
2022-07-06 12:08:22 -0400

    Working Jinja rendering with validation

--- xmlConfigBuilder.py
+++ xmlConfigBuilder.py
@@ -3,34 +3,37 @@ from jinja2 import Template
 from uci import XMLParser
 
 
-def main(xml_template, widow_data, output_file):
-    with open(widow_data) as file:
-        data = json.load(file)
-    
-    with open(xml_template) as file:
+def main():
+    # Open Widow Data and mapping file
+    # Build a mapped data dict
+    # Open Jinja2 template
+    # Render the Jinja2 template with the mapped data dict
+    # Write the rendered template to a file
+    # Validate the written file with XMLParser
+
+    mapped_data = {}
+
+    with open("amtiWidowData.json") as file:
+        widow_data = json.load(file)
+
+    with open("amtiExampleMapping.json") as file:
+        mapping = json.load(file)
+
+    for key in widow_data:
+        mapped_data[mapping[key]] = widow_data[key]
+
+    with open("amtiExample.j2") as file:
         template = Template(file.read())
+    
+    rendered_template = template.render(mapped_data)
 
-    # root = XMLParser().parse_object(f.read())
+    with open("amtiExampleRendered.xml", "w") as file:
+        file.write(rendered_template)
 
-    with open(output_file, "w") as rendered_template:
-        rendered_template.write(
-            template.render(template_mapping(data))
-        )
-
-
-def template_mapping(data):
-    return {
-        "capability_id_uuid": data["capability_id"],
-        "capability_type": data["capability_product_type"],
-        "sub_capability_type": data["sub_capability_123_type"],
-        "accepted_interface": data["interface"],
-        "interrupt_other_activities": data["interrupt_activities"],
-        "collection_policy": data["collection_111_policy"],
-        "current_ops_supported": data["concurrency-supported"],
-        "supp_freq_min": data["minimum_frequency"],
-        "supp_freq_max": data["maximum_frequency"]
-    }
+    with open("amtiExampleRendered.xml", "rb") as file:
+        root = XMLParser().parse_object(file.read())
+        print(root)
 
 
 if __name__ == "__main__":
-    main("amtiExample.j2", "amtiWidowData.json", "amtiExampleRendered.xml")
+    main()
